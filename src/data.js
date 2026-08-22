9672aa0c817e1ba7d02104af2a2ad37045d40f7f
unknown <matiassimonluduena@gmail.com>
2026-08-22 00:33:34 -0300

    main - base

---
+++ src/data.js
@@ -0,0 +1,17 @@
+export const data = {
+  HTML: {
+    id: 1,
+    title: "HTML",
+    steps: [
+      {
+        step: 1,
+        texts: [
+          "HTML significa Lenguaje de Marcado de Hipertexto o por sus siglas en inglés, HyperText Markup Language. Es el código que define la estructura y el contenido de una página web. Este es tu editor de código, donde escribirás HTML.",
+          "Busca la línea 1 en el editor y escribe este texto:",
+          "- Bienvenido a UpCode",
+          'Cuando hayas terminado, haz clic en el botón "Comprobar el código" para ver si es correcto.',
+        ],
+      },
+    ],
+  },
+};
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,5 +1,28 @@
+import "./main.css";
+
+import { data } from "../data";
+
 const Main = () => {
-  return <main>main</main>;
+  console.log(data.HTML);
+
+  const lesson = "HTML";
+  const step = 1;
+
+  return (
+    <main>
+      <h3>{data[lesson].title}</h3>
+      <div className="main-boxes">
+        <div className="main-box">
+          <h4>Paso {data[lesson].steps[step - 1].step}</h4>
+          {data[lesson].steps[step - 1].texts.map((text, index) => (
+            <p key={index}>{text}</p>
+          ))}
+        </div>
+        <div className="main-box"></div>
+        <div className="main-box"></div>
+      </div>
+    </main>
+  );
 };
 
 export default Main;
