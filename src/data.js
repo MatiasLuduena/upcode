7e5db2feebb43fa3f63ec87f987de03e22e3ac8e
unknown <matiassimonluduena@gmail.com>
2026-08-23 20:44:23 -0300

    main - basic logic

--- src/data.js
+++ src/data.js
@@ -9,7 +9,13 @@ export const data = {
           "HTML significa Lenguaje de Marcado de Hipertexto o por sus siglas en inglés, HyperText Markup Language. Es el código que define la estructura y el contenido de una página web. Este es tu editor de código, donde escribirás HTML.",
           "Busca la línea 1 en el editor y escribe este texto:",
           "- Bienvenido a UpCode",
-          'Cuando hayas terminado, haz clic en el botón "Comprobar el código" para ver si es correcto.',
+          'Cuando hayas terminado, haz clic en el botón "Comprobar" para ver si es correcto.',
+        ],
+      },
+      {
+        step: 2,
+        texts: [
+          "¡Felicidades! Has completado el primer paso de HTML. Ahora, vamos a aprender sobre etiquetas y cómo estructurar tu contenido.",
         ],
       },
     ],
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -1,5 +1,5 @@
 import "./main.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { data } from "../data";
 
@@ -7,9 +7,11 @@ import Editor from "@monaco-editor/react";
 
 const Main = () => {
   const [editorValue, setEditorValue] = useState("");
+  const [step, setStep] = useState(1);
+  const [expectedCode, setExpectedCode] = useState("");
+  const [isCodeIncorrect, setIsCodeIncorrect] = useState(false);
 
   const lesson = "HTML";
-  const step = 1;
 
   const preview = `
   <!DOCTYPE html>
@@ -21,29 +23,64 @@ const Main = () => {
     </body>
   </html>`;
 
+  const validateCode = () => {
+    if (editorValue.trim().toLocaleLowerCase() === expectedCode) {
+      setStep(step + 1);
+      setIsCodeIncorrect(false);
+      setEditorValue("");
+    } else {
+      setIsCodeIncorrect(true);
+    }
+  };
+
+  useEffect(() => {
+    data[lesson].steps[step - 1].texts.map((text) => {
+      if (text.startsWith("-")) {
+        setExpectedCode(text.slice(1).trim().toLocaleLowerCase());
+      }
+    });
+  }, [step]);
+
   return (
     <main>
       <h3>{data[lesson].title}</h3>
       <div className="main-boxes">
         <div className="main-box">
           <h4>Paso {data[lesson].steps[step - 1].step}</h4>
-          {data[lesson].steps[step - 1].texts.map((text, index) => (
-            <p key={index}>{text}</p>
-          ))}
+          {data[lesson].steps[step - 1].texts.map((text, index) => {
+            if (text.startsWith("-")) {
+              return (
+                <p key={index} className="main-box_text">
+                  {text.slice(1).trim()}
+                </p>
+              );
+            }
+            return <p key={index}>{text}</p>;
+          })}
         </div>
         <div className="main-box">
-          <Editor
-            height="100%"
-            language={lesson.toLowerCase()}
-            theme="vs-dark"
-            value={editorValue}
-            onChange={setEditorValue}
-            options={{
-              minimap: {
-                enabled: false,
-              },
-            }}
-          />
+          <div className="main-box_editor">
+            <Editor
+              height="100%"
+              language={lesson.toLowerCase()}
+              theme="vs-dark"
+              value={editorValue}
+              onChange={setEditorValue}
+              options={{
+                minimap: {
+                  enabled: false,
+                },
+              }}
+            />
+          </div>
+          <button className="btn btn-primary" onClick={validateCode}>
+            Comprobar
+          </button>
+          {isCodeIncorrect && (
+            <p className="error-message">
+              Código incorrecto! Inténtalo de nuevo.
+            </p>
+          )}
         </div>
         <div className="main-box">
           <iframe srcDoc={preview} title="preview" />
