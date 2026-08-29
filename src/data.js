141a59cfcb400313effe2a1e0bab7f604180c9e3
unknown <matiassimonluduena@gmail.com>
2026-08-29 02:12:55 -0300

    main html workshop-one

--- src/data.js
+++ src/data.js
@@ -1,23 +1,76 @@
 export const data = {
-  HTML: {
-    id: 1,
-    title: "HTML",
-    steps: [
-      {
-        step: 1,
-        texts: [
-          "HTML significa Lenguaje de Marcado de Hipertexto o por sus siglas en inglés, HyperText Markup Language. Es el código que define la estructura y el contenido de una página web. Este es tu editor de código, donde escribirás HTML.",
-          "Busca la línea 1 en el editor y escribe este texto:",
-          "- Bienvenido a UpCode",
-          'Cuando hayas terminado, haz clic en el botón "Comprobar" para ver si es correcto.',
-        ],
-      },
-      {
-        step: 2,
-        texts: [
-          "¡Felicidades! Has completado el primer paso de HTML. Ahora, vamos a aprender sobre etiquetas y cómo estructurar tu contenido.",
-        ],
-      },
-    ],
-  },
+  HTML: [
+    {
+      id: 1,
+      title: "HTML",
+      steps: [
+        {
+          step: 1,
+          texts: [
+            "<span>HTML</span> significa Lenguaje de Marcado de Hipertexto o por sus siglas en inglés, HyperText Markup Language. Es el código que define la estructura y el contenido de una página web. Este es tu editor de código, donde escribirás HTML.",
+            "Busca la línea 1 en el editor y escribe este texto:",
+            "- Bienvenido a UpCode",
+            "Cuando hayas terminado, haz clic en el botón <span>Comprobar</span> para ver si es correcto.",
+          ],
+        },
+        {
+          step: 2,
+          texts: [
+            "<span>HTML</span> se compone de elementos. La mayoría de los elementos tienen una etiqueta de apertura <span>&lt;&gt;</span> y una etiqueta de cierre <span>&lt;/&gt;</span>. Entre estas dos etiquetas se encuentra el contenido, que puede ser texto u otros elementos HTML. El primero que utilizarás es el elemento <span>h1</span>",
+            "Comienza con la etiqueta de apertura <span>&lt;h1&gt;</span>, termina con la etiqueta de cierre <span>&lt;/h1&gt;</span>, y tiene el texto que mostrará entre las etiquetas:",
+            "- <h1>Bienvenido a UpCode</h1>",
+            "Convierte tu <span>Bienvenido a UpCode</span> en un elemento <span>h1</span> agregando una etiqueta de apertura delante y una etiqueta de cierre detras.",
+          ],
+        },
+        {
+          step: 3,
+          texts: [
+            "Observa que el código <span>HTML</span> que escribas en el editor aparecerá en la vista previa. En este taller, escribirás el código HTML para una página web que sera la portada de tu portafolio como desarrollador web.",
+            "Debajo de tu elemento <span>h1</span>, escribe lo siguiente en la línea vacía:",
+            "- Mi portafolio de desarrollador web",
+          ],
+        },
+        {
+          step: 4,
+          texts: [
+            "Un elemento <span>h1</span> es el encabezado principal de una página web y solo debes usar uno por página. Los elementos <span>h2</span> representan subencabezados. Puedes tener varios por página y se ven así:",
+            "- <h2>Mi portafolio de desarrollador web</h2>",
+            "Convierte el texto <span>Mi portafolio de desarrollador web</span> en un elemento <span>h2</span> agregando las etiquetas de apertura y cierre.",
+          ],
+        },
+        {
+          step: 5,
+          texts: [
+            "Cuando necesite agregar un párrafo a una página web, puede usar el elemento <span>p</span> de esta manera:",
+            "- <p>Aprende a como convertirte en un desarrollador web</p>",
+            "Añade el texto <span>Aprende a como convertirte en un desarrollador web</span> en un elemento <span>p</span> agregando las etiquetas de apertura y cierre.",
+          ],
+        },
+        {
+          step: 6,
+          texts: [
+            "Los elementos <span>p</span> pueden contener otros elementos, como el elemento <span>strong</span>, que se utiliza para resaltar texto en negrita. Y <span>em</span> que se utiliza para resaltar texto en cursiva. Se ven así:",
+            "- <p>Hoy <strong>aprenderas</strong> a usar <em>HTML</em></p>",
+            "Rodea la palabra <span>aprenderas</span> con las etiquetas de apertura y cierre del elemento <span>strong</span> y la palabra <span>HTML</span> con las etiquetas de apertura y cierre del elemento <span>em</span>.",
+          ],
+        },
+        {
+          step: 7,
+          texts: [
+            "En HTML hay seis elementos de encabezado: del <span>h1</span> al <span>h6</span>. Se utilizan para mostrar la importancia de las secciones de tu página web, siendo <span>h1</span> el más importante y <span>h6</span> el menos importante.",
+            "Debajo de tu elemento <span>p</span>, agrega un encabezado <span>h3</span> con el texto:",
+            "- <h3>Introducción a HTML</h3>",
+          ],
+        },
+        {
+          step: 8,
+          texts: [
+            "Los elementos <span>h1</span> a <span>h6</span> y <span>p</span> son elementos de bloque, lo que significa que ocupan todo el ancho disponible y comienzan en una nueva línea. En cambio, los elementos <span>strong</span> y <span>em</span> son elementos en línea, lo que significa que solo ocupan el espacio necesario para su contenido y no comienzan en una nueva línea.",
+            "Puedes probar la diferencia de tamaño entre los elementos <span>h1</span> a <span>h6</span> en la vista previa. Observa que el elemento <span>h3</span> es más pequeño que el elemento <span>h2</span> y más grande que el elemento <span>p</span>. Esto se debe a que los elementos de encabezado tienen un tamaño de fuente predeterminado mayor que los elementos de párrafo.",
+            "Finalizaste el primer taller de HTML. Haz clic en el botón <span>Finalizar</span> para volver al menu principal y continuar con el siguiente.",
+          ],
+        },
+      ],
+    },
+  ],
 };
--- src/main/Main.jsx
+++ src/main/Main.jsx
@@ -6,13 +6,20 @@ import { data } from "../data";
 import Editor from "@monaco-editor/react";
 
 const Main = () => {
+  // Valor del editor de código
   const [editorValue, setEditorValue] = useState("");
+  // Paso actual del taller
   const [step, setStep] = useState(1);
-  const [expectedCode, setExpectedCode] = useState("");
+  // Código esperado para el paso actual
+  const [expectedCode, setExpectedCode] = useState([]);
+  // Estado para indicar si el código ingresado es incorrecto
   const [isCodeIncorrect, setIsCodeIncorrect] = useState(false);
+  // Lección actual
+  const [lesson, setLesson] = useState("HTML");
+  // Taller actual
+  const [workshop, setWorkshop] = useState(0);
 
-  const lesson = "HTML";
-
+  // Generar la vista previa del código HTML ingresado en el editor
   const preview = `
   <!DOCTYPE html>
   <html>
@@ -23,31 +30,49 @@ const Main = () => {
     </body>
   </html>`;
 
+  // Función para validar el código ingresado en el editor
   const validateCode = () => {
-    if (editorValue.trim().toLocaleLowerCase() === expectedCode) {
+    const userCode = editorValue
+      .trim()
+      .toLowerCase()
+      .split("\n")
+      .map((line) => line.trim());
+
+    const expected = expectedCode.map((code) => code.trim().toLowerCase());
+
+    if (
+      userCode.length === expected.length &&
+      userCode.every((code, index) => code === expected[index])
+    ) {
       setStep(step + 1);
       setIsCodeIncorrect(false);
-      setEditorValue("");
     } else {
       setIsCodeIncorrect(true);
     }
   };
 
+  // Actualizar el código esperado cuando cambie el paso
   useEffect(() => {
-    data[lesson].steps[step - 1].texts.map((text) => {
-      if (text.startsWith("-")) {
-        setExpectedCode(text.slice(1).trim().toLocaleLowerCase());
-      }
-    });
+    const expected = data[lesson][workshop].steps[step - 1].texts
+      .filter((text) => text.startsWith("-"))
+      .map((text) => text.slice(1).trim().toLowerCase());
+
+    setExpectedCode((prev) => [
+      ...prev.filter((code) => code.startsWith("<")),
+      ...expected,
+    ]);
   }, [step]);
 
   return (
     <main>
-      <h3>{data[lesson].title}</h3>
+      {/* Mostrar el título del taller actual */}
+      <h3>{data[lesson][workshop].title}</h3>
+      {/* Contenedor de las cajas */}
       <div className="main-boxes">
-        <div className="main-box">
-          <h4>Paso {data[lesson].steps[step - 1].step}</h4>
-          {data[lesson].steps[step - 1].texts.map((text, index) => {
+        {/* Caja de instrucciones */}
+        <div className="main-box main-box_texts">
+          <h4>Paso {data[lesson][workshop].steps[step - 1].step}</h4>
+          {data[lesson][workshop].steps[step - 1].texts.map((text, index) => {
             if (text.startsWith("-")) {
               return (
                 <p key={index} className="main-box_text">
@@ -55,14 +80,17 @@ const Main = () => {
                 </p>
               );
             }
-            return <p key={index}>{text}</p>;
+            return (
+              <p key={index} dangerouslySetInnerHTML={{ __html: text }}></p>
+            );
           })}
         </div>
+        {/* Caja del editor de código */}
         <div className="main-box">
           <div className="main-box_editor">
             <Editor
               height="100%"
-              language={lesson.toLowerCase()}
+              language={lesson.toLocaleLowerCase()}
               theme="vs-dark"
               value={editorValue}
               onChange={setEditorValue}
@@ -82,6 +110,7 @@ const Main = () => {
             </p>
           )}
         </div>
+        {/* Caja de vista previa */}
         <div className="main-box">
           <iframe srcDoc={preview} title="preview" />
         </div>
